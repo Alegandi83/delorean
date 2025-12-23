1d2bdc58d5406355af7799f396396a43c72dc633
Alegandi83 <alessandrogandini@hotmail.com>
2025-12-23 13:02:11 +0100

    added apitube schema

--- azure/solutions/adb-platform-cablemaster-assets/ws-producer/das_information_technologies.tf
+++ azure/solutions/adb-platform-cablemaster-assets/ws-producer/das_information_technologies.tf
@@ -6,3 +6,10 @@ resource "databricks_catalog" "das_information_technologies" {
   force_destroy   = true
   isolation_mode  = "ISOLATED"
 }
+
+resource "databricks_schema" "apitube" {
+  depends_on    = [databricks_catalog.das_information_technologies]
+  catalog_name  = databricks_catalog.das_information_technologies.name
+  name          = "apitube"
+  force_destroy = true
+}
